1026a285077d9941932bf0fa3b578e235d7883ee
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-03 17:27:26 +0530

    Style header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <header className="row block center">
+      <div>
+        <a href="#/">
+          <h1>Simple Shopping Cart</h1>
+        </a>
+      </div>
+      <div>
+        <a href="#/cart">Cart</a>&nbsp;
+        <a href="#/signin">Sign In</a>
+      </div>
+    </header>
+  );
 }
