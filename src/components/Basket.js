e511a05823555ef1c6ea5c022eb77cf864596595
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-03 19:48:25 +0530

    Calculate total price in the cart

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 export default function Basket({ cartItems, onAdd, onRemove }) {
+  const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+  const taxPrice = itemsPrice * 0.18;
+  const shippingPrice = itemsPrice > 2000 ? 0 : 50;
+  const totalPrice = itemsPrice + taxPrice + shippingPrice;
+
   return (
     <aside className="block col-1">
       <h2>Cart Items</h2>
@@ -21,6 +26,31 @@ export default function Basket({ cartItems, onAdd, onRemove }) {
           </div>
         </div>
       ))}
+      {cartItems.length !== 0 && (
+        <>
+          <hr />
+          <div className="row">
+            <div className="col-2">Items Price</div>
+            <div className="col-1 text-right">${itemsPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">Tax Price</div>
+            <div className="col-1 text-right">${taxPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">Shipping Price</div>
+            <div className="col-1 text-right">${shippingPrice.toFixed(2)}</div>
+          </div>
+          <div className="row">
+            <div className="col-2">
+              <strong>Total Price</strong>
+            </div>
+            <div className="col-1 text-right">
+              <strong>${totalPrice.toFixed(2)}</strong>
+            </div>
+          </div>
+        </>
+      )}
     </aside>
   );
 }
