8c4252b29bf8bece7c4edb4b9e2db6aad40963af
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-03 19:38:52 +0530

    Add item to the cart

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import Basket from './components/Basket';
@@ -5,12 +6,28 @@ import data from './data';
 
 function App() {
   const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+
+    if (exist) {
+      // If product exists in the cart already, update its quantity
+      setCartItems(cartItems.map((x) => (x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x)));
+    } else {
+      // If product does not exist in the cart, add it
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+
+  const onRemove = () => {};
+
   return (
     <div className="App">
       <Header></Header>
       <div className="row">
-        <Main products={products}></Main>
-        <Basket></Basket>
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket cartItems={cartItems} onAdd={onAdd} onRemove={onRemove}></Basket>
       </div>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Product from './Product';
 
-export default function Main({ products }) {
+export default function Main({ products, onAdd }) {
   return (
     <main className="block col-2">
       <h2>Products</h2>
       <div className="row">
         {products.map((product) => {
-          return <Product key={product.id} product={product} />;
+          return <Product key={product.id} product={product} onAdd={onAdd} />;
         })}
       </div>
     </main>
