4d820b2ebab4ccb511c8357b82d2aeff8a0984cf
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-03 19:41:35 +0530

    Remove item from the shopping cart

--- src/App.js
+++ src/App.js
@@ -20,7 +20,15 @@ function App() {
     }
   };
 
-  const onRemove = () => {};
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(cartItems.map((x) => (x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x)));
+    }
+  };
 
   return (
     <div className="App">
